824654459f41e9779f009271068f0b2c8c1797d9
Gašper Novak <125385025+gaspernovak20@users.noreply.github.com>
2026-03-11 14:51:33 +0100

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,37 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(f"Customize Your Smoothie! :cup_with_straw:")
+st.write("Chose the fruits you want in your custom Smoothie!")
+
+name_of_order = st.text_input('Name of Smoothie:')
+st.write('The name of Smoothie will be:', name_of_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list = st.multiselect(
+    'Chose up to 5 ingrediants:'
+    ,my_dataframe
+    ,max_selections=5
+)
+
+ingredients_string = ''
+
+if ingredients_list:   
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                    values ('""" + ingredients_string + """','""" + name_of_order + """')"""
+    st.write(my_insert_stmt)
+    
+    time_to_insert = st.button('Submit Order')
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
