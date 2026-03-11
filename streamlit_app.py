4cd1fb82e0417ebf5a18116af9d79024299acd42
Gašper Novak <125385025+gaspernovak20@users.noreply.github.com>
2026-03-11 15:48:51 +0100

    Comment out dataframe display and convert to pandas
    Comment out the dataframe display and convert to pandas.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,11 @@ st.write('The name of Smoothie will be:', name_of_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+pf_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
